6644ba272f0e3124211865cabeefcb0d783fdc30
MatiasSpirilis <69637630+MatiasSpirilis@users.noreply.github.com>
2024-05-24 23:19:58 +0000

    updated roles

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -8,18 +8,24 @@ CREATE ROLE 'web_writer';
 
 -- Asigno permisos a los roles
 
-GRANT ALL ON web_db.* TO 'web_admin';
-GRANT SELECT ON web_db.* TO 'web_reader';
-GRANT INSERT, UPDATE, DELETE ON web_db.* TO 'web_writer';
+GRANT ALL ON coderhouse_hr.* TO 'web_admin';
+GRANT SELECT ON coderhouse_hr.* TO 'web_reader';
+GRANT INSERT, UPDATE, DELETE ON coderhouse_hr.* TO 'web_writer';
 
 -- Creo usuarios y asigno roles
 
-CREATE USER 'admin_user'@'localhost' IDENTIFIED BY 'admin_pass';
-CREATE USER 'reader_user1'@'localhost' IDENTIFIED BY 'reader1_pass';
-CREATE USER 'reader_user2'@'localhost' IDENTIFIED BY 'reader2_pass';
-CREATE USER 'writer_user1'@'localhost' IDENTIFIED BY 'writer1_pass';
+CREATE USER 'admin_user'@'%' IDENTIFIED BY 'admin_pass';
+CREATE USER 'reader_user1'@'%' IDENTIFIED BY 'reader1_pass';
+CREATE USER 'reader_user2'@'%' IDENTIFIED BY 'reader2_pass';
+CREATE USER 'writer_user1'@'%' IDENTIFIED BY 'writer1_pass';
 
-GRANT 'app_developer' TO 'dev1'@'localhost';
-GRANT 'app_read' TO 'read_user1'@'localhost';
-GRANT 'app_read' TO 'read_user2'@'localhost';
-GRANT 'app_write' TO 'rw_user1'@'localhost';
+GRANT 'web_admin' TO 'admin_user'@'%';
+GRANT 'web_reader' TO 'reader_user1'@'%';
+GRANT 'web_reader' TO 'reader_user2'@'%';
+GRANT 'web_writer' TO 'writer_user1'@'%';
+
+GRANT ALL ON coderhouse_hr.* TO 'admin_user'@'%';
+GRANT SELECT ON coderhouse_hr.* TO 'reader_user1'@'%';
+GRANT SELECT ON coderhouse_hr.* TO 'reader_user2'@'%';
+GRANT INSERT, UPDATE, DELETE ON coderhouse_hr.* TO 'writer_user1';
+FLUSH PRIVILEGES;
